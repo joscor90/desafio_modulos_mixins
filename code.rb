df9df40a142bfdf5d707b0198374cedf72d0fb80
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-08 10:35:39 -0300

    refactored code using new classes

---
+++ code.rb
@@ -0,0 +1,48 @@
+class Person
+    def initialize(first, last, age)
+        @first_name = first
+        @last_name = last
+        @age = age
+    end
+
+    def birthday
+        @age += 1
+    end
+
+    def talk
+    end
+
+    def introduce
+    end
+end
+
+
+class Student < Person
+    def talk
+        puts "Aquí es la clase de programación con Ruby?"
+    end
+
+    def introduce
+        puts "Hola profesor. Mi nombre es #{self.first_name} #{self.last_name}"
+    end
+end
+
+class Teacher < Person
+    def talk
+        puts "Bienvenidos a la clase de programación con Ruby!"
+    end
+
+    def introduce
+        puts "Hola alumnos. Mi nombre es #{self.first_name} #{self.last_name}."
+    end
+end
+
+class Parent < Person
+    def talk 
+        puts "Aquí es la reunión de apoderados?"
+    end
+
+    def introduce
+        puts "Hola. Soy uno de los apoderados. Mi nombre es #{self.first_name} #{self.last_name}."
+    end
+end
